82e9da13e06e0c96df747f2a941f4ab1c668ade6
jonghoon kim <balfouri1210@gmail.com>
2019-12-05 20:50:44 +0900

    Refactor: for sonny

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const STAGE = process.env.STAGE || 'local';
 const dotEnv = require('dotenv');
 dotEnv.config();
 
 const app = express();
 app.get('/', (req, res, next) => {
-  res.send(`Hello from node server! NOS API ${process.env.STAGE} is served by EC2 and managed by CODEPIPELINE. REALLY? Yeah, sure it is ~`);
+  res.send(`I LOVE 소니`);
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+  console.log(process.env[`DB_URL_${STAGE}`]);
 });
