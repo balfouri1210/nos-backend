c63530b02e85319994d6df6209b27d0b1fa15d21
jonghoon kim <balfouri1210@gmail.com>
2019-12-08 15:09:49 +0900

    Refactor: allow cors for all of request

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+var cors = require('cors');
 const STAGE = process.env.STAGE || 'local';
 const dotEnv = require('dotenv');
+
 dotEnv.config();
+app.use(cors()); // Allow cors
 
 const app = express();
 app.get('/', (req, res, next) => {
