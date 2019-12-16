605397c421b1c842efd7761d89c3faa0f63ba54f
jonghoon kim <balfouri1210@gmail.com>
2019-12-16 19:27:15 +0900

    Refactor: test connection with prod db

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ const express = require('express');
 var cors = require('cors');
 const STAGE = process.env.STAGE || 'local';
 const dotEnv = require('dotenv');
+dotEnv.config(); // Use .env file
 
-const app = express();
-dotEnv.config();
-
+const app = express(); // Generate express app
 app.use(cors()); // Allow cors
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: process.env[`DB_URL_${STAGE}`],
+  user: "admin",
+  password: "kjh236874",
+  database: "nos",
+});
+
 app.get('/', (req, res, next) => {
-  res.send(`되냐 이제?`);
+  connection.query("select * from user", function(err, result, fields) {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
