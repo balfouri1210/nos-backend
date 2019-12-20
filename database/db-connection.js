aeac118bad3ef4784cc01eefdbb1d8462dc468c9
jonghoon kim <balfouri1210@gmail.com>
2019-12-20 13:01:31 +0900

    Refactor: add port to db connection param

--- database/db-connection.js
+++ database/db-connection.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env[`DB_URL_${process.env.STAGE}`],
+  port: 3306,
   user: 'admin',
   password: 'kjh236874',
   database: 'nos'
