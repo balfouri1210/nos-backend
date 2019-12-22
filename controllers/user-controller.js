c012d1e81a6eb8cc94390965a534f8249aea955c
jonghoon kim <balfouri1210@gmail.com>
2019-12-22 10:58:21 +0900

    Refactor: rollback to mysql2

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,21 +1,10 @@
 const userService = require('../services/user-service');
 const { defaultServerResponse } = require('../constants/index');
 
-// module.exports.getUser = async (req, res) => {
-//   try {
-//     const result = await userService.getUser(req.body);
-//     res.send(result);
-//   } catch (err) {
-//     defaultServerResponse.message = err.message;
-//     res.send(defaultServerResponse);
-//   }
-// };
-
-module.exports.getUser = (req, res) => {
+module.exports.getUser = async (req, res) => {
   try {
-    userService.getUser(req.body, (result) => {
-      res.send(result);
-    });
+    const result = await userService.getUser(req.body);
+    res.send(result);
   } catch (err) {
     defaultServerResponse.message = err.message;
     res.send(defaultServerResponse);
--- services/user-service.js
+++ services/user-service.js
@@ -1,32 +1,11 @@
-// const pool = require('../database/db-connection');
-const mysql = require('mysql');
+const pool = require('../database/db-connection');
 
-// module.exports.getUser = async (serviceData) => {
-//   try {
-//     const [rows] = await pool.query('select * from user');
-//     return rows;
-//   } catch (err) {
-//     err.message = 'Query failed!';
-//     return err;
-//   }
-// };
-
-module.exports.getUser = (serviceData, callback) => {
-  const connection = mysql.createConnection({
-    host: process.env[`DB_URL_${process.env.STAGE}`],
-    port: 3306,
-    user: 'admin',
-    password: 'kjh236874',
-    database: 'nos'
-  });
-
-  // connection.query('SELECT * FROM user', function (err, rows, fields) {
-  //   if (!err) {
-  //     callback(rows);
-  //   } else {
-  //     err.message = 'Query Failed!!!!';
-  //     throw err;
-  //   }
-  // });
-  callback(connection);
+module.exports.getUser = async (serviceData) => {
+  try {
+    const [rows] = await pool.query('select * from user');
+    return rows;
+  } catch (err) {
+    err.message = 'Query failed!';
+    return err;
+  }
 };
