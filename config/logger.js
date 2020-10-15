be5fb58b1dfb84017e8c35a3d25572316b80fc37
jonghoon kim <balfouri1210@gmail.com>
2020-10-15 12:06:21 +0900

    Refactor: change config of logger property

--- config/logger.js
+++ config/logger.js
@@ -1,7 +1,16 @@
+// Datadog 로깅 구현 참고자료들
+// https://docs.datadoghq.com/logs/log_collection/nodejs/?tab=winston30
+// https://docs.datadoghq.com/logs/processing/attributes_naming_convention/
+// https://github.com/winstonjs/winston/blob/master/docs/transports.md#datadog-transport
+// https://velog.io/@ash/Node.js-%EC%84%9C%EB%B2%84%EC%97%90-logging-%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%9F%AC%EB%A6%AC-winston-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0
+
+// 원래 winston은 로그 파일을 생성하는 용도로 많이 사용하지만
+// 나는 datadog 문서를 참고하여 파일생성이 아니라 datadog에 직접 로그를 등록하는 방식으로 구현했다.
+// 파일을 생성하고, datadog으로 파싱할수도 있는것으로 보이지만 별로 필요성을 못느꼈다.
+
 const winston = require('winston');
 const requestIp = require('request-ip');
 
-const logDir = 'logs';  // logs 디렉토리 하위에 로그 파일 저장
 const { combine, timestamp, printf } = winston.format;
 
 const httpTransportOptions = {
@@ -21,14 +30,12 @@ const createLog = function(type, message, req) {
       // 'network.client.ip': req.headers['x-forwarded-for'] || req.connection.remoteAddress,
       // 'network.client.geoip': req.headers['x-forwarded-for'] || req.connection.remoteAddress,
       'network.client.ip': requestIp.getClientIp(req),
-      'network.client.geoip': requestIp.getClientIp(req),
       'http.method': req.method,
       'http.useragent': req.headers['user-agent']
     });
   } else if (type === 'error') {
     logger.error(message, {
       'network.client.ip': requestIp.getClientIp(req),
-      'network.client.geoip': requestIp.getClientIp(req),
       'http.method': req.method,
       'http.useragent': req.headers['user-agent']
     });
@@ -49,14 +56,14 @@ const logger = winston.createLogger({
 
   defaultMeta: { service: 'user-service' },
 
+  // 로그등록
   transports: [
     new winston.transports.Http(httpTransportOptions)
-    // new winston.transports.File({ filename: `${logDir}/combined.log` })
   ],
 
+  // unhandled error를 알아서 캐치해서 등록해준다
   exceptionHandlers: [
     new winston.transports.Http(httpTransportOptions)
-    // new winston.transports.File({ filename: `${logDir}/exceptions.log` })
   ]
 });
 
